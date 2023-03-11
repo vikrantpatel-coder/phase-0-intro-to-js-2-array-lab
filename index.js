72782bf3510aae2361df486c4b50223578e99a13
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-03-11 10:16:53 -0500

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+    function destructivelyAppendCat(name){
+        return cats.push(name); 
+    
+    }
+
+    function destructivelyPrependCat(name){
+        return cats.unshift (name)
+    }
+
+    function  destructivelyRemoveLastCat(name){
+        return cats.pop(name)
+    }
+
+    function destructivelyRemoveFirstCat(name){
+        return cats.shift(name)
+    }
+
+    function appendCat(name){
+        const newCats = [...cats, name];
+        return newCats;
+      
+    }
+    
+    function prependCat(name){
+        const newCats = [name,...cats];
+        return newCats;
+    
+    }
+
+    function removeLastCat(name){
+        const newCats = cats.slice(0,cats.length-1, name);
+        return newCats;
+    }
+
+    function removeFirstCat(name){
+        const newCats = cats.slice(1, name);
+        return newCats;
+    }
+
+
+
+
+
